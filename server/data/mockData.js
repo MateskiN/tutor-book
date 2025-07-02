6953986f039f3e0699729f40407a34f884000e1f
nmateski <nikola.mateski@sapienzaconsulting.com>
2025-07-02 09:39:27 +0200

    removed commented code

--- server/data/mockData.js
+++ server/data/mockData.js
@@ -6,13 +6,6 @@ const tutors = [
   { id: 4, name: 'Tutor 4', subjects: ['Physics', 'Biology'], hourlyRate: 35 }
 ];
 
-// const availability = [
-//   { tutorId: 1, date: '2025-07-02', slots: ['09:00', '10:00', '11:00'] },
-//   { tutorId: 2, date: '2025-07-02', slots: ['13:00', '14:00'] },
-//   { tutorId: 3, date: '2025-07-02', slots: ['10:00', '15:00', '16:00'] },
-//   { tutorId: 4, date: '2025-07-02', slots: ['12:00', '13:00', '18:00'] }
-// ];
-
 const bookings = [];
 
 export default { tutors, bookings };
